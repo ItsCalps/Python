d784133cd67069ea4a1664145e9866794a301cdb
ItsCalps <liottomarc@gmail.com>
2026-07-22 23:05:09 +0200

    Delete gestor_de_invitados.py

--- gestor_de_invitados.py
+++
@@ -1,9 +0,0 @@
-
-lista = ["carlos", "marc", "maria", "lucas", "lucia"]
-
-invitados = input("Que invitados hay en la lista? ").lower()
-
-if invitados in lista:
-    print("Ese invitado esta en la lista!")
-else:
-    print("Ese invitado no esta en la lista!")
