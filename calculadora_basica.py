51c2a2894a19c11dc9ba4da9e115f8f932f015ed
ItsCalps <liottomarc@gmail.com>
2026-07-22 22:59:25 +0200

    Delete calculadora_basica.py

--- calculadora_basica.py
+++
@@ -1,6 +0,0 @@
-numero_1 = float(input("¿Que numero quieres multiplicar? "))
-numero_2 = float(input("¿Por que numero lo quieres multiplicar? "))
-
-resultado = numero_1 * numero_2
-
-print(resultado)
