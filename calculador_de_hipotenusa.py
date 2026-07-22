f0edf352c3716439794deae22d35f4cb0d2e8288
ItsCalps <liottomarc@gmail.com>
2026-07-22 22:59:37 +0200

    Delete calculador_de_hipotenusa.py

--- calculador_de_hipotenusa.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Escribe la longitud del lado corto: "))
-
-b = int(input("Escribe la longitud del otro lado corto: "))
-
-c = ((a ** 2) + (b ** 2)) ** 0.5
-
-print("La longitud de la hipotenusa es:", c)
